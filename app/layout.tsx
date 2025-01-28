a4b3ee65ddcfb8eaed0e25b8ccd10110b80cd70a
Jijo Jacob <Jijojacob988@gmail.com>
2025-01-28 13:39:56 +0530

    code modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.classname} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         <ThemeProvider
         attribute="class"
